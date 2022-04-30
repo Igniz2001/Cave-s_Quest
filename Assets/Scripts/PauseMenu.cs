8102dae991d240d9bd95d155d4bb69f9a97e5945
Igniz <valenciasamuel037@gmail.com>
2022-04-30 03:47:32 -0500

    Menú de Pausa
    Menú de pausa implementado

---
+++ Assets/Scripts/PauseMenu.cs
@@ -0,0 +1,49 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+public class PauseMenu : MonoBehaviour
+{
+    [SerializeField] private GameObject pauseButton;
+    [SerializeField] private GameObject pauseMenu;
+    [SerializeField] private GameObject ScoreCanvas;
+    public bool pause = false;
+
+    private void Update()
+    {
+        if (Input.GetKeyDown(KeyCode.Escape))
+        {
+            if (pause)
+            {
+                Resume();
+            }
+            else
+            {
+                GamePause();
+            }
+        }
+    }
+
+    public void GamePause()
+    {
+        pause = true;
+        Time.timeScale = 0f;
+        pauseButton.SetActive(false);
+        pauseMenu.SetActive(true);
+    }
+
+    public void Resume()
+    {
+        pause = false;
+        Time.timeScale = 1f;
+        pauseButton.SetActive(true);
+        pauseMenu.SetActive(false);
+    }
+
+    public void Quit()
+    {
+        Time.timeScale = 1f;
+        SceneManager.LoadScene("PrincipalMenu");
+    }
+
+}

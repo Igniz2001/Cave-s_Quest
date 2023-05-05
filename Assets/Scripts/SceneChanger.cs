5e98f5a52d2998d2ba39a1675020f6b7842b3f7c
Samael <samael041203@gmail.com>
2023-05-05 13:10:20 -0500

    mini menus removidos
    se pasa derecho al siguiente nivel

--- Assets/Scripts/SceneChanger.cs
+++ Assets/Scripts/SceneChanger.cs
@@ -6,12 +6,13 @@ using UnityEngine.SceneManagement;
 public class SceneChanger : MonoBehaviour
 {
     // Start is called before the first frame update
+    public string scene;
     private void OnTriggerEnter2D(Collider2D other)
     {
         if (other.gameObject.tag == "Hero")
         {
             print("Has ganado el nivel");
-            SceneManager.LoadScene("LevelPassed");
+            SceneManager.LoadScene(scene);
         }
     }
 }
